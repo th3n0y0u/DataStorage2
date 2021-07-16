b74c9de5e56b2ebec7256955c68f3819d3128da0
soop <77305497+th3n0y0u@users.noreply.github.com>
2021-07-16 15:37:38 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -20,6 +20,38 @@ game.Players.PlayerAdded:Connect(function(player)
 	
 	if success then
 		Buttons.Value = data
+		--[[
+			-- This is for model loading via leaderstats
+			for v = 1, Buttons.Value, 1 do
+				if Buttons.Value ~= 0 then
+					local part = game.Workspace["Part "..v]
+					if v == data.Buttons then
+						local spawnlocation = game.Workspace.SpawnLocation
+						game.Workspace["Part 1"].Button1.Button.BillboardGui.Parent = part["Button"..v].Button
+						spawnlocation.CFrame = CFrame.new(part["Button"..v].Button.Position)
+						spawnlocation.Size = Vector3.new(part["Button"..v].Button.Size) 
+						if v == 1 then
+							game.Workspace.SpawnLocation.CanTouch = false
+						end
+					end
+				else
+					
+					while wait() do
+						if game.Workspace.Loading == false then
+							game.Workspace.SpawnLocation.CanTouch = true
+							break
+						end
+					end
+					
+				end
+			end
+			
+			if Buttons.Value > 0 then
+				for i, v in pairs(game.Players:GetPlayers()) do
+					v.Character:BreakJoints()
+				end
+			end
+		--]]
 	end
 end)
 
